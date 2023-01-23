b31a1cb49737c3d678415e406708282e01c7f675
Gian Karl L. Colinares <111488754+Gkcoli@users.noreply.github.com>
2023-01-23 08:46:14 +0800

    Update App.java

--- Notes-3/App.java
+++ Notes-3/App.java
@@ -1,17 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        //5! = 5 * (4 * 3 * 2 * 1) = 120
-        System.out.println(factorial(10));
+  public class shyshy {
+	
+	public static void main(String[] args) {
+		 // this part will prompt the user to input a factorial that you like.
+		 //5! = 5 * (4 * 3 * 2 * 1) = 120
+        System.out.println(factorial(5));// enter the factorial here
        }
-    public static int factorial(int n){
+    public static int factorial(int n){// call the method this may be used later on
         if(n == 1){
            System.out.println("factorial(" + n + ") = 1");
            return 1;
         }
-        else{
+        else{// this will add the desired factorial that you input
             System.out.println("factorial("+ n +  ") = " + n + "* factorial(" + (n - 1) + ")");
-            return n * factorial(n - 1);
+            return n * factorial(n - 1);// if this code works it's a miracle
         }
     }
 }   
